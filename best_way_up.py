ce3a5a0d5a7da8abb3d455a5fa24df87da1b864a
= <=>
2025-03-11 22:12:00 +0300

    find the shortest combination up the stairs

---
+++ best_way_up.py
@@ -0,0 +1,12 @@
+from typing import List
+def best_way_up(stairs: int, steps: List[int]) -> List[int]:
+    """_Find the shortest combinations of steps up the stairs_
+
+    Args:
+        stairs (int): _Number of stairs_
+        steps (List[int]): _Number of stairs you are allowed to climb at a time_
+
+    Returns:
+        List[int]: _List of shortest way up_
+    """
+    ...
