368c956401d229fe1c4b6aec7be740c232daa63e
= <=>
2025-03-11 23:10:43 +0300

    finding best way up using recursion

--- best_way_up.py
+++ best_way_up.py
@@ -1,5 +1,5 @@
 from typing import List
-def best_way_up(stairs: int, steps: List[int]) -> List[int]:
+def best_way_up_recur(stairs: int, steps: List[int]) -> List[int]:
     """_Find the shortest combinations of steps up the stairs_
 
     Args:
@@ -9,4 +9,26 @@ def best_way_up(stairs: int, steps: List[int]) -> List[int]:
     Returns:
         List[int]: _List of shortest way up_
     """
-    ...
+    best_way: List[int] = []
+    if stairs == 0:
+        return []
+    elif stairs < 0:
+        return None
+    else:
+        for step in steps:
+            new_stairs: int = stairs - step
+            result: List[int] = best_way_up_recur(stairs= new_stairs, steps= steps)
+            if result != None:
+                current = [step] + result
+                if len(current) < len(best_way) or len(best_way) == 0:
+                    best_way = current
+    return best_way
+
+def main()-> None:
+    steps: List[int] = [2, 1]
+    stairs: int = 5
+    best_way: List[int] = best_way_up_recur(stairs=stairs, steps= steps)
+    print(f"The best way up {stairs} stairs restricted to  one of {steps} steps at a time is {best_way} ")
+
+if __name__ == "__main__":
+    main()
